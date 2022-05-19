d9e184fe989771fd52a24c49e6dd853e8fe3e74d
cjy <cjyoun95@naver.com>
2022-05-19 16:12:46 +0900

    점심 고르기 / ROOM 라이브러리 적용 및 설정, preference util 생성

---
+++ app/src/main/java/com/example/kotlinpractice/DAO/choiceLaunch/PersonDAO.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinpractice.DAO.choiceLaunch
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.kotlinpractice.entity.choiceLaunch.PersonEntity
+
+// interface 위에 어노테이션 Dao 선언
+@Dao
+interface PersonDAO {
+
+    // Query 어노테이션으로 해당 값 가져오기
+    @Query("SELECT * FROM tb_person")
+    fun getAll(): List<PersonEntity>
+
+    // 여러개 insert 할 수도 있기 때문에 vararg로 선언
+    @Insert
+    fun insertAll(vararg personEntity: PersonEntity)
+
+    @Delete
+    fun delets(personEntity: PersonEntity)
+
+}
---
+++ app/src/main/java/com/example/kotlinpractice/appDB/choiceLaunch/ChoiceLaunchAppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.kotlinpractice.appDB.choiceLaunch
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kotlinpractice.DAO.choiceLaunch.PersonDAO
+import com.example.kotlinpractice.entity.choiceLaunch.PersonEntity
+
+// Database 어노테이션 추가
+// PersonEntity를 사용하며 version은 1, 스키마를 추출하지 않음으로 설정.
+@Database(entities = [PersonEntity::class], version = 1, exportSchema = false)
+abstract class ChoiceLaunchAppDatabase : RoomDatabase(){
+    abstract fun personDao() : PersonDAO
+
+    // 어디서든 접근 가능하고 중복 생성되지 않도록 싱글톤으로 companion object로 생성
+    companion object{
+        private var instance: ChoiceLaunchAppDatabase? =null
+
+        @Synchronized
+        fun getInstance(context: Context): ChoiceLaunchAppDatabase?{
+            if(instance==null){
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ChoiceLaunchAppDatabase::class.java,
+                    "database-choiceLaunch"  // DB이름은 database-choiceLaunch,
+                )
+                    .allowMainThreadQueries()   // UI Thread(mainThread)에서 접근 가능하게 설정
+                    .build()
+            }
+            return instance
+        }
+    }
+}
