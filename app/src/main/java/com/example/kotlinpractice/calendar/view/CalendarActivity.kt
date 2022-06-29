b06e4e2ddc7263199ac78de4814c977a138029cf
cjy <cjyoun95@naver.com>
2022-06-29 18:03:15 +0900

    캘린더 / 날짜 입력 로직 추가

--- app/src/main/java/com/example/kotlinpractice/calendar/view/CalendarActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/calendar/view/CalendarActivity.kt
@@ -13,6 +13,7 @@ import android.widget.Toast
 import com.example.kotlinpractice.BaseActivity
 import com.example.kotlinpractice.databinding.ActivityCalendarBinding
 import java.text.SimpleDateFormat
+import java.time.Year
 import java.util.*
 
 class CalendarActivity : BaseActivity() {
@@ -56,45 +57,8 @@ class CalendarActivity : BaseActivity() {
 
         // 달력 날짜 선택 시
         binding.calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
-
-            // 선택한 날짜 yyyy/MM/dd 형식으로 파싱
-            val format = SimpleDateFormat("yyyy/MM/dd")
-            val date = format.parse("$year/${month+1}/$dayOfMonth")
-            val dateTime = date.time
-            Log.d("time1", date.toString())
-            // 선택한 날짜가 무슨 요일인지 구하기
-            val simpleDateFormat = SimpleDateFormat("E요일", Locale.KOREAN) // 요일 나오게 하는 패턴 (한글로 변경)
-            val dayName: String = simpleDateFormat.format(date) // 선택한 날의 요일일
-
-            // 날짜를 보여주는 텍스트에 해당 날짜를 넣는다.
-            val selectDay = String.format("%d / %d / %d", year, month + 1, dayOfMonth) + " ($dayName)"
-            Log.d("time2", selectDay)
-            binding.dDay.text = selectDay
-
-            // 오늘 날짜 0시0분으로 맞추기
-            val today = Calendar.getInstance().apply {
-                set(Calendar.HOUR_OF_DAY,0)
-                set(Calendar.MINUTE,0)
-                set(Calendar.SECOND,0)
-                set(Calendar.MILLISECOND,0)
-            }
-            val oneDay:Long = 60 * 60 * 24 * 1000   // type을 명시해주어야만 계산이 가능함 타입추론으로 Int값이 Long값으로 변하게 되면 다른 값이 계산될 수 있음
-            val ingDay =((today.time.time - dateTime) / oneDay ) + 1    // 오늘과 선택한 날의 차이를 통해 몇일이 지났는지 계산, 첫날부터 1일로 치기 때문에 +1
-
-            Log.d("time3", "${today.time.time} / $dateTime / $oneDay / $ingDay")
-
-            // 100일 단위로 10000일까지 구하기
-            var str = ""
-            for(i in 100 .. 10000 step 100){
-                val addDay = dateTime + ((i-1) * oneDay)
-                date.time = addDay
-                val afterDay = format.format(date) // 선택한 날의 100일 후
-                val afterDayName = simpleDateFormat.format(date) // 선택한 날의 100일 후 요일
-                str = "$str$i 일 - $afterDay ($afterDayName)\n"
-            }
-
-            // 선택한 날짜 이후의 날들을 출력
-            binding.dayList.text = "$ingDay 일 째 \n$str"
+            Log.d("day num - ", "year : $year / month : $month / dayOfMonth : $dayOfMonth")
+            setDayList(year, month, dayOfMonth)
         }
 
         binding.dayList.movementMethod = ScrollingMovementMethod()  // textView scrollbar 가능하게 하기 xml에 해당 부분에 andriod:scrollbars="vertical" 추가
@@ -105,10 +69,59 @@ class CalendarActivity : BaseActivity() {
                 Toast.makeText(this,"날짜를 제대로 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else{
                 val inputDay:String = binding.inputDay.text.toString()
-
+                val inputDayYear = inputDay.substring(0 until 4).toInt()
+                val inputDayMonth = inputDay.substring(4 until 6).toInt() - 1
+                val inputDayDay = inputDay.substring(6).toInt()
+                Log.d("inputDay num - ", "year : $inputDayYear / month : $inputDayMonth / dayOfMonth : $inputDayDay")
+                setDayList(inputDayYear, inputDayMonth, inputDayDay)
             }
         }
 
+    }
+
+    // 날짜 계산 함수
+    private fun setDayList(year: Int, month: Int, dayOfMonth: Int){
+        binding.dayList.text = "" // 초기화
+        binding.dDay.text = ""
+
+        // 선택한 날짜 yyyy/MM/dd 형식으로 파싱
+        val format = SimpleDateFormat("yyyy/MM/dd")
+        val date = format.parse("$year/${month+1}/$dayOfMonth")
+        val dateTime = date.time
+        Log.d("time1", date.toString())
+        // 선택한 날짜가 무슨 요일인지 구하기
+        val simpleDateFormat = SimpleDateFormat("E요일", Locale.KOREAN) // 요일 나오게 하는 패턴 (한글로 변경)
+        val dayName: String = simpleDateFormat.format(date) // 선택한 날의 요일일
+
+        // 날짜를 보여주는 텍스트에 해당 날짜를 넣는다.
+        val selectDay = String.format("%d / %d / %d", year, month + 1, dayOfMonth) + " ($dayName)"
+        Log.d("time2", selectDay)
+        binding.dDay.text = selectDay
+
+        // 오늘 날짜 0시0분으로 맞추기
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY,0)
+            set(Calendar.MINUTE,0)
+            set(Calendar.SECOND,0)
+            set(Calendar.MILLISECOND,0)
+        }
+        val oneDay:Long = 60 * 60 * 24 * 1000   // type을 명시해주어야만 계산이 가능함 타입추론으로 Int값이 Long값으로 변하게 되면 다른 값이 계산될 수 있음
+        val ingDay =((today.time.time - dateTime) / oneDay ) + 1    // 오늘과 선택한 날의 차이를 통해 몇일이 지났는지 계산, 첫날부터 1일로 치기 때문에 +1
+
+        Log.d("time3", "${today.time.time} / $dateTime / $oneDay / $ingDay")
+
+        // 100일 단위로 10000일까지 구하기
+        var str = ""
+        for(i in 100 .. 10000 step 100){
+            val addDay = dateTime + ((i-1) * oneDay)
+            date.time = addDay
+            val afterDay = format.format(date) // 선택한 날의 100일 후
+            val afterDayName = simpleDateFormat.format(date) // 선택한 날의 100일 후 요일
+            str = "$str$i 일 - $afterDay ($afterDayName)\n"
+        }
+
+        // 선택한 날짜 이후의 날들을 출력
+        binding.dayList.text = "$ingDay 일 째 \n$str"
     }
 
 }
