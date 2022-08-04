1cd04a61193e1a7c3c474a593431d6134b873806
cjyoun <cjyoun95@naver.com>
2022-08-04 21:09:45 +0900

    지도 현재 위치 , 카메라 이동 포인트

--- app/src/main/java/com/example/kotlinpractice/navermap/view/NaverMapActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/navermap/view/NaverMapActivity.kt
@@ -1,20 +1,35 @@
 package com.example.kotlinpractice.navermap.view
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.kotlinpractice.MainActivity
-import com.example.kotlinpractice.databinding.ActivityMainBinding
-import com.example.kotlinpractice.databinding.ActivityNaverMapBinding
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import android.Manifest
 
-class NaverMapActivity : AppCompatActivity(), OnMapReadyCallback {
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.example.kotlinpractice.BaseActivity
+import com.example.kotlinpractice.MainActivity
+import com.example.kotlinpractice.databinding.ActivityNaverMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+
+class NaverMapActivity : BaseActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
 
     private lateinit var naverMap: NaverMap     // 지도 기능 사용을 위한 콜백 등록
+
+    // 지도의 기능 사용을 위한 선언
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationSource: FusedLocationSource
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
 
     private lateinit var binding: ActivityNaverMapBinding    // 전역으로 바인딩 객체 생성 (Activity별 자동 생성된 Binding클래스 매칭)
 
@@ -32,15 +47,104 @@ class NaverMapActivity : AppCompatActivity(), OnMapReadyCallback {
             startActivity(goHome)
         }
 
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         // 지도 기능 사용을 위한 콜백 등록
+        // getMapAsync를 호출하여 비동기로 onMapReady 콜백 메서드 호출
+        // onMapReady에서 NaverMap 객체를 받음
         mapView.getMapAsync(this)
+
+
 
 
     }
 
     // 지도 기능 사용을 위한 콜백 등록
+    // 지도 세팅
     override fun onMapReady(map: NaverMap){
-        naverMap = map
+        this.naverMap = map
+
+        // 지도 줌 설정
+        naverMap.maxZoom = 18.0 // 최대 줌 설정 (가까워짐)
+        naverMap.minZoom = 5.0  // 최소 줌 설정 (멀어짐)
+
+        //내장 위치 추적 기능 사용
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        // 마커 (네이버맵 현재 가운데에 항상 위치)
+        val marker = Marker()
+        marker.position = LatLng(
+            naverMap.cameraPosition.target.latitude,
+            naverMap.cameraPosition.target.longitude
+        )
+        marker.map = naverMap
+
+
+        // fusedLocationClient.lastLocation 을 사용하기 위한 permission 체크
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        // 사용자 현재 위치 받아오기
+        var currentLocation: Location?
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                currentLocation = location
+                // 위치 오버레이의 가시성은 기본적으로 false로 지정되어 있습니다. 가시성을 true로 변경하면 지도에 위치 오버레이가 나타납니다.
+                // 파랑색 점, 현재 위치 표시
+                naverMap.locationOverlay.run {
+                    isVisible = true
+                    position = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+                }
+
+                // 카메라 현재위치로 이동
+                val cameraUpdate = CameraUpdate.scrollTo(
+                    LatLng(
+                        currentLocation!!.latitude,
+                        currentLocation!!.longitude
+                    )
+                )
+                naverMap.moveCamera(cameraUpdate)
+
+                // 빨간색 마커 현재위치로 변경
+                marker.position = LatLng(
+                    naverMap.cameraPosition.target.latitude,
+                    naverMap.cameraPosition.target.longitude
+                )
+            }
+
+
+
+        // 카메라의 움직임에 대한 이벤트 리스너 인터페이스.
+        // 참고 : https://navermaps.github.io/android-map-sdk/reference/com/naver/maps/map/package-summary.html
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            Log.i("NaverMap", "카메라 변경 - reson: $reason, animated: $animated")
+            marker.position = LatLng(
+                // 현재 보이는 네이버맵의 정중앙 가운데로 마커 이동
+                naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude
+            )
+
+        }
+
+        // 카메라의 움직임 종료에 대한 이벤트 리스너 인터페이스.
+        naverMap.addOnCameraIdleListener {
+            marker.position = LatLng(
+                naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude
+            )
+
+        }
+
     }
 
 
